32de90b3b685f9fc14aeca22d95521eea2763ea3
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-06-23 11:49:45 +0900

    Fix test on CI

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ requires = [
 development_requires = [
     'Babel',
     'flake8',
+    'pylint',
     'waitress',
 ]
 
@@ -49,39 +50,41 @@ cmdclass = {}
 cmdclass = cmdclass.copy()
 cmdclass.update(babel_cmdclass)
 
-setup(name='tirol',
-      version='0.1',
-      description='The IntROduction website for ScroLliris.',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-      ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      extras_require={
-          'development': development_requires,
-          'testing': testing_requires,
-          'production': production_requires,
-      },
-      install_requires=requires,
-      message_extractors={'tirol': [
-          ('**.py', 'python', None),
-          ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
-          ('static/**', 'ignore', None),
-      ]},
-      cmdclass=cmdclass,
-      entry_points="""\
-      [paste.app_factory]
-      main = tirol:main
-      [console_scripts]
-      tirol_pserve = tirol.scripts.pserve:main
-      tirol_pstart = tirol.scripts.pstart:main
-      """)
+setup(
+    name='tirol',
+    version='0.1',
+    description='The IntROduction website for ScroLliris.',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web wsgi pylons pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'development': development_requires,
+        'testing': testing_requires,
+        'production': production_requires,
+    },
+    install_requires=requires,
+    message_extractors={'tirol': [
+        ('**.py', 'python', None),
+        ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
+        ('static/**', 'ignore', None),
+    ]},
+    cmdclass=cmdclass,
+    entry_points="""\
+    [paste.app_factory]
+    main = tirol:main
+    [console_scripts]
+    tirol_pserve = tirol.scripts.pserve:main
+    tirol_pstart = tirol.scripts.pstart:main
+    """,
+)
