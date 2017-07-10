43e3b77f725449ffe9e2fe2894d56d2ccc39499f
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-07-10 22:31:58 +0900

    Fix scroll transition
    * Fix scrollTop on Chromium
    * Update threshold value

--- tirol/assets/js/script.js
+++ tirol/assets/js/script.js
@@ -4,9 +4,13 @@ window.addEventListener('scroll', () => {
     , html = document.documentElement
     ;
 
+  // NOTE:
+  // Chromium does not have `html.scrollTop` (use `body.scrollTop`)
   let percentage = Math.round(100 *
-    Math.abs(html.scrollTop / (html.scrollHeight - html.clientHeight)));
-  if (percentage >= 66) {
+    Math.abs((html.scrollTop || body.scrollTop) /
+             (html.scrollHeight - html.clientHeight)));
+
+  if (percentage >= 40) {
     if (body.classList.contains('fixed')) {
       body.classList.remove('fixed');
     }
