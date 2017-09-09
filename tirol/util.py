bcf10656979e9a0576d4ac9dca48ec9c4babe39c
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-09-10 05:20:06 +0900

    Fix type annotation style

--- tirol/util.py
+++ tirol/util.py
@@ -1,3 +1,5 @@
+"""Utility module.
+"""
 from bleach import clean as _clean
 from markupsafe import Markup
 
@@ -6,19 +8,20 @@ from pyramid.events import BeforeRender
 
 
 @subscriber(BeforeRender)
-def add_template_utilities(evt) -> None:
-    """Adds template utilities`.
+def add_template_utilities(evt):  # type: (dict) -> None
+    """Adds utility functions for templates.
     """
     evt['clean'] = clean
 
-def clean(**kwargs) -> 'function':
+
+def clean(**kwargs):  # type: (**dict) -> 'function'
     """Returns sanitized value except allowed tags and attributes.
 
     >>> ${'<a href="/"><em>link</em></a>'|n,clean(
             tags=['a'], attributes=['href'])}
     "<a href=\"/\">link</a>"
     """
-    def __clean(text) -> Markup:
+    def __clean(text):  # type: (str) -> Markup
         return Markup(_clean(text, **kwargs))
 
     return __clean
