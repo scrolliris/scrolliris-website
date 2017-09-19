5ffdcdce70543090a8cc33e13e5021caa449b553
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-09-19 23:16:27 +0900

    Update pstart script

--- tirol/scripts/pstart.py
+++ tirol/scripts/pstart.py
@@ -10,27 +10,30 @@ from tirol.env import Env
 
 
 def usage(argv):
+    """Print pstart command usage
+    """
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s {staging|production}.ini")' % (cmd, cmd))
     sys.exit(1)
 
 
-def main(argv=sys.argv, quiet=False):
-    """ start server process. """
+def main(argv=sys.argv, _quiet=False):
+    """Starts main production server process
+    """
     if len(argv) < 2:
         usage(argv)
 
     Env.load_dotenv_vars()
     env = Env()
 
-    config = argv[1]
-    wsgi_app = get_app(config)
-    setup_logging(config)
+    config_file = argv[1] if 1 in argv else 'config/production.ini'
+    wsgi_app = get_app(config_file)
+    setup_logging(config_file)
 
     from paste.script.cherrypy_server import cpwsgi_server
-    cpwsgi_server(wsgi_app, host=env.host, port=env.port,
-                  numthreads=10, request_queue_size=100)
+    return cpwsgi_server(wsgi_app, host=env.host, port=env.port,
+                         numthreads=10, request_queue_size=100)
 
 
 if __name__ == '__main__':
