359590ed4cc0e8c060c9c8c63fc233e9dc7b1dc7
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-10-06 00:29:30 +0900

    Remove Babel (use xgettext)

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ import os
 from setuptools import setup, find_packages
 
 # pylint: disable=invalid-name
-try:
-    from babel.messages import frontend as babel
-    babel_cmdclass = {
-        'extract_messages': babel.extract_messages,
-    }
-except ImportError:
-    babel_cmdclass = {}
-
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, *('doc', 'DESCRIPTION.rst'))) as f:
     DESCRIPTION = f.read()
@@ -35,7 +27,6 @@ requires = [
 
 development_requires = [
     'colorlog',
-    'Babel',
     'flake8',
     'pylint',
     'waitress',
@@ -52,10 +43,6 @@ testing_requires = [
 production_requires = [
     'CherryPy',
 ]
-
-cmdclass = {}
-cmdclass = cmdclass.copy()
-cmdclass.update(babel_cmdclass)
 
 setup(
     name='tirol',
@@ -86,7 +73,7 @@ setup(
         ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
         ('static/**', 'ignore', None),
     ]},
-    cmdclass=cmdclass,
+    cmdclass={},
     entry_points="""\
     [paste.app_factory]
     main = tirol:main
