8255b0c97ef368e35b73b0ab3538934e0e9c58ff
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-12-14 18:03:05 +0900

    Update NOTE

--- thun/scripts/pserve.py
+++ thun/scripts/pserve.py
@@ -5,8 +5,10 @@ from thun.env import Env
 
 def main(argv=None, quiet=False):
     """Runs original pserve with .env support."""
+    # NOTE:
     # `pserve` (PServeCommand) needs `hupper`, it has dependency to **fcntl**.
     # In some environment (e.g. app engine), fcntl is not found :'(
+    # So, that's why this import is in method.
     from pyramid.scripts.pserve import PServeCommand
 
     if not argv:
